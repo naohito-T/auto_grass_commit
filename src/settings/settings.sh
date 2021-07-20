bdca35793c26f40698e9f876619fbaa575a8aace
Naohito-T <naohito.tanaka0523@gmail.com>
2021-07-20 10:10:40 +0900

    fix code

--- src/settings/settings.sh
+++ src/settings/settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ファイル書き込み内容settings
-declare -A writeOneLines = (
+declare -A writeOneLines=(
     [1]="fix code${uuid}"
     [2]="fix code${uuid}"
     [3]="fix code${uuid}"
@@ -12,18 +12,18 @@ declare -A writeOneLines = (
 )
 
 # commit message settings
-declare -A commitMsgs = (
-    [1]=`feature/fix_code_shell${uuid}`
-    [2]=`feature/fix_code_shell${uuid}`
-    [3]=`feature/fix_code_shell${uuid}`
-    [4]=`feature/fix_code_shell${uuid}`
-    [5]=`feature/fix_code_shell${uuid}`
-    [6]=`feature/fix_code_shell${uuid}`
-    [7]=`feature/fix_code_shell${uuid}`
+declare -A commitMsgs=(
+    [1]="feature/fix_code_shell${uuid}"
+    [2]="feature/fix_code_shell${uuid}"
+    [3]="feature/fix_code_shell${uuid}"
+    [4]="feature/fix_code_shell${uuid}"
+    [5]="feature/fix_code_shell${uuid}"
+    [6]="feature/fix_code_shell${uuid}"
+    [7]="feature/fix_code_shell${uuid}"
 )
 
 # 曜日と回数の連想配列 settings
-declare -A commitWeeks = (
+declare -A commitWeeks=(
     [1]="5"
     [2]="5"
     [3]="5"
